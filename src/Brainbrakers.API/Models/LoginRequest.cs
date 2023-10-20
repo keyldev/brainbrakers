5e42482087a99469ddbd27d4ab0539957d809c0c
keyldev <wudgexyz@gmail.com>
2023-10-20 23:01:24 +0300

    models from api project removed

--- src/Brainbrakers.API/Models/LoginRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace podcast_api.Models
-{
-    public class LoginRequest
-    {
-        public string Username { get; set; }
-        public string Password { get; set; }
-
-    }
-}
--- src/Brainbrakers.API/Models/RefreshTokenResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace podcast_api.Models
-{
-    public class RefreshTokenResponse
-    {
-        public string RefreshToken { get; set; }
-        public string AccessToken { get; set; }
-    }
-}
