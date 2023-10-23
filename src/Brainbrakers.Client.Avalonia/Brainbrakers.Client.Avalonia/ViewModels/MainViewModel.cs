76e11becc3b91302e6100f86665b0c25245d4b97
keyldev <wudgexyz@gmail.com>
2023-10-23 22:18:57 +0300

    added avalonia test clients (need to move these to another tree)

---
+++ src/Brainbrakers.Client.Avalonia/Brainbrakers.Client.Avalonia/ViewModels/MainViewModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Brainbrakers.Client.Avalonia.ViewModels;
+
+public class MainViewModel : ViewModelBase
+{
+    public string Greeting => "Welcome to Avalonia!";
+}
---
+++ src/Brainbrakers.Client.Avalonia/Brainbrakers.Client.Avalonia/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Brainbrakers.Client.Avalonia.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
