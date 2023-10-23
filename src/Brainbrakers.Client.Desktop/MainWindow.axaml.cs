be8b10b7cb8c2ef14702c26c4ecb8e85a9f35137
keyldev <wudgexyz@gmail.com>
2023-10-23 20:26:03 +0300

    Avalonia test client for this app

---
+++ src/Brainbrakers.Client.Desktop/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Brainbrakers.Client.Desktop
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
