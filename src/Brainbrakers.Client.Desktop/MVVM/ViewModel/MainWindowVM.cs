29c0ae23f7747cef67fd8fe400b1937e45d586ad
keyldev <wudgexyz@gmail.com>
2023-10-25 19:58:39 +0300

    [fix]: git conflict

---
+++ src/Brainbrakers.Client.Desktop/MVVM/ViewModel/MainWindowVM.cs
@@ -0,0 +1,18 @@
+﻿using Brainbrakers.Client.Desktop.Core;
+using System.Diagnostics;
+
+namespace Brainbrakers.Client.Desktop.MVVM.ViewModel;
+
+public class MainWindowVM
+{
+
+    public RelayCommand ShowDebugMessage { get; set; }
+
+    public MainWindowVM()
+    {
+        ShowDebugMessage = new RelayCommand(o =>
+        {
+            Debug.WriteLine("Some message");
+        });
+    }
+}
--- src/Brainbrakers.Client.Desktop/MainWindow.axaml.cs
+++ src/Brainbrakers.Client.Desktop/MainWindow.axaml.cs
@@ -1,4 +1,5 @@
 using Avalonia.Controls;
+using Brainbrakers.Client.Desktop.MVVM.ViewModel;
 
 namespace Brainbrakers.Client.Desktop
 {
@@ -7,6 +8,7 @@ namespace Brainbrakers.Client.Desktop
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainWindowVM();
         }
     }
 }
