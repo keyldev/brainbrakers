ebbaa70518fc45554c5642563111dbc882a8a595
keyldev <wudgexyz@gmail.com>
2023-10-29 18:09:38 +0300

    feat: loginview

---
+++ src/Brainbrakers.Client.Desktop/MVVM/View/LoginView.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace Brainbrakers.Client.Desktop.MVVM.View;
+
+public partial class LoginView : UserControl
+{
+    public LoginView()
+    {
+        InitializeComponent();
+    }
+}
